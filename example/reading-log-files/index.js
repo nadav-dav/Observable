f7fc4302ec37ed414da4f81f3dae2f3a0771e6c0
Nadav Leshem <nadavl@wix.com>
2015-01-20 19:31:14 +0200

    added log files process example

---
+++ example/reading-log-files/index.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var readline = require('readline');
+var Observable = require("../../");
+
+
+var filesInFolder   = readDir(__dirname);
+var logFiles        = filesInFolder.filter(isAccessLog);
+var logLines        = logFiles.map(readFileLines);
+var logEntries      = logLines.map(parseLine);
+var errorEntries    = logEntries.filter(isErrorEntry);
+
+errorEntries.subscribe(console.log);
+
+
+function parseLine (line) {
+	var regex = /^([0-9\.]+(?:, [0-9\.]+)*) \S+ \S+ \[([^\]]+)\] "([A-Z]+) ([^"]*) HTTP\/(\S+)" (\d+) (\S+) "[^"]*" "([^"]*)"/;
+	var parts = line.match(regex);
+	var i = 0;
+    return {
+        ips: parts[++i],
+        date: parts[++i],
+        method: parts[++i],
+        path: parts[++i],
+        httpVersion: parts[++i],
+        status: parseInt(parts[++i]),
+        size: parseInt(parts[++i]),
+        userAgent: parts[++i]
+    };
+}
+
+function readFileLines (file, emit) {
+    readline
+        .createInterface({ input: fs.createReadStream(file), output: process.stdout, terminal: false})
+        .on('line', function(line) { emit(line); });
+}
+
+function isAccessLog (filename){
+	return filename.toLowerCase().substr(-11) === ".access.log"
+}
+function isErrorEntry (entry){
+    return entry.status >= 400
+}
+
+function readDir (path) {
+    var s = new Observable.create();
+    fs.readdir(path, function(err, filesArray){
+        s.emit(filesArray);
+    });
+    return s.flatten();
+}
