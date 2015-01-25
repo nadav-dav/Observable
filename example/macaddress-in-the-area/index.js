93c7196e8259db53573234ec82e1a26956257251
Nadav Leshem <nadavl@wix.com>
2015-01-25 10:53:15 +0200

    whitespaces

--- example/macaddress-in-the-area/index.js
+++ example/macaddress-in-the-area/index.js
@@ -3,15 +3,18 @@ var readline = require('readline');
 var Observable = require("../../");
 var macvendor = require('macvendor');
 
+var WIRELESS_INTERFACE = "en0";
+var SUBNET = "10.0.0.0";
 
-var readingDump = command('tcpdump', '-I -e -i en0'.split(" "));
+
+var readingDump = command('tcpdump', ('-I -e -i ' + WIRELESS_INTERFACE).split(" "));
 
 var raRequests = readingDump
     .filter(contains(/RA\:/))
     .map(extractRaMacAddress)
     .map(lookupMacAddressVendor);
 
-var ipsInNetwork = command("nmap","10.0.0.1/24 -sn".split(" "))
+var ipsInNetwork = command("nmap", (SUBNET + "/24 -sn").split(" "))
     .filter(contains(/Nmap scan report for/))
     .map(extractIp);
 
@@ -29,60 +32,60 @@ raRequests.on("data", console.log);
 //});
 
 
-function readBeaconName(line){
+function readBeaconName(line) {
     var regex = /Beacon \((.+)\)/;
     var parts = line.match(regex);
     return parts[1];
 }
 
-function extractRaMacAddress(line){
+function extractRaMacAddress(line) {
     var regex = /RA:([^ ]+)/;
     var parts = line.match(regex);
     return parts[1];
 }
 
-function extractIp(line){
+function extractIp(line) {
     var regex = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/;
     var parts = line.match(regex);
     return parts[1];
 }
 
-function lookupMacAddressVendor(data, out){
-    macvendor(data, function(err, vendor) {
+function lookupMacAddressVendor(data, out) {
+    macvendor(data, function (err, vendor) {
         if (err) return out.error(err);
-        out.send([data,vendor]);
+        out.send([data, vendor]);
     });
 }
 
-function aggregateUnique (timeout){
+function aggregateUnique(timeout) {
     var collectedData = [];
     var _out;
-    setInterval(function(){
-        if (_out){
+    setInterval(function () {
+        if (_out) {
             _out.send(collectedData.sort());
             collectedData = [];
         }
-    },timeout);
-    
-    return function(data, out){
+    }, timeout);
+
+    return function (data, out) {
         _out = out;
-        if (collectedData.indexOf(data) === -1){
+        if (collectedData.indexOf(data) === -1) {
             collectedData.push(data);
         }
     }
 }
 
-function contains(regex){
-    return function(data){
+function contains(regex) {
+    return function (data) {
         return data.match(regex) != null;
     }
 }
-function command (cmd, params){
+function command(cmd, params) {
     var s = Observable.create();
     var running = spawn(cmd, params);
 
     readline
-        .createInterface({ input : running.stdout, output : process.stdout, terminal : false})
+        .createInterface({input: running.stdout, output: process.stdout, terminal: false})
         .on('line', function (line) {
             s.send(line);
         })
@@ -92,11 +95,11 @@ function command (cmd, params){
 
     running.on('close', function (code, signal) {
         if (code === 0) s.end();
-        else s.error(new Error('child process terminated due to receipt of signal '+signal));
+        else s.error(new Error('child process terminated due to receipt of signal ' + signal));
     });
 
     running.on('disconnect', function () {
-        s.error(new Error('Command \''+cmd+'\' got disconnected'));
+        s.error(new Error('Command \'' + cmd + '\' got disconnected'));
     });
     return s;
 }
